e7b8934210bde5fe58e73448a84f46406d96ed55
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-08-01 21:31:41 -0300

    Adicionado dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRoute from './src/routes/user.route.js';
 import connectDatabase from './src/database/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000
 
+dotenv.config();
 connectDatabase();
 app.use(express.json());
 app.use("/api", userRoute);
--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
+
 const connectDatabase = () => {
     console.log("Waiting Connection...");
 
-    mongoose.connect("mongodb+srv://root:root@cluster0.xz5uzvv.mongodb.net/?retryWrites=true&w=majority",
+    mongoose.connect(process.env.MONGODB_URI,
         { useNewUrlParser: true, useUnifiedTopology: true }
     ).then(() => console.log("MongoDB Atlas Connected!")).catch((error) => console.log(error));
     
